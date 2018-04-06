80e74f0644277e207557a6cd5661189a5b290ff6
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-06 19:31:22 +0530

    Update popup.py

--- popup.py
+++ popup.py
@@ -1,5 +1,8 @@
 import ctypes  # An included library with Python install.
 import time
 
+
 def popup(alarm_text):
     ctypes.windll.user32.MessageBoxW(0, alarm_text, "Alarm Alarm Alarm !!!!", 0)
+
+# so simple :P
