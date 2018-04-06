1492d0224dd6b3b44c04428e9b026e9d5d064c23
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-06 19:22:56 +0530

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -1,16 +1,28 @@
+# <--------https://github.com/thirstycode/priority-alarm------>
+# <-------TODO---- good looking gui ------>
 from tkinter import *
 import time
 from datetime import datetime as dt
 import main2
+
+# tkinter 
 win = Tk()
 win.title("Priority Alarm")
 
+# required lists
 OPTIONS_YEAR = [2018,2019,2020]
 OPTIONS_MONTH = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 OPTIONS_DATE = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 OPTIONS_HOUR = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 OPTIONS_MINUTE = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+priorities = [
+    ("PopUp Window",1),
+    ("SoundPlay Of Alarm",2),
+    ("Shutdown Your Computer",3),
+]
 
+
+# required variables
 output1 = StringVar()
 variable_year = StringVar(win)
 variable_year.set(dt.now().year)
