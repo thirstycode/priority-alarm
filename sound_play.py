ac1abc5a1cf0a127f3b00c17dc90a1c35648ac54
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-06 19:33:41 +0530

    *_*

--- sound_play.py
+++ sound_play.py
@@ -6,3 +6,5 @@ def sound_play(soundtext):
     tts = gTTS(text='Alarm Alert. Alarm Alert.' + soundtext + soundtext, lang='en')
     tts.save("good.mp3")
     playsound("good.mp3")
+
+# her voice *_*
